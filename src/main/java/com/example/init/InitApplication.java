4105eb344a6d13fce90323621ec8362833e499ae
zhangqingfu <15195988693@163.com>
2021-04-17 19:36:56 +0800

    更新

--- src/main/java/com/example/init/InitApplication.java
+++ src/main/java/com/example/init/InitApplication.java
@@ -1,11 +1,9 @@
 package com.example.init;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.init.mapper")
 public class InitApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/example/init/InitApplicationTests.java
+++ src/test/java/com/example/init/InitApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.init;
 
-import cn.hutool.crypto.SecureUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,8 +9,6 @@ class InitApplicationTests {
     @Test
     void contextLoads() {
 
-        String admin123 = SecureUtil.md5("admin123");
-        System.out.println(admin123);
     }
 
 }
